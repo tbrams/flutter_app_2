e29d5e695d3867a422f0a2dbaa3b8b8323bcf001
tbrams <torben@brams.dk>
2019-01-04 23:04:30 +0100

    Experimented with styling on the auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -17,47 +17,71 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(.5), BlendMode.dstATop),
+          ),
         ),
-        body: Container(
-            margin: EdgeInsets.all(10.0),
-            child: ListView(children: <Widget>[
-              TextField(
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (String value) {
-                    setState(() {
-                      _emailValue = value;
-                    });
-                  }),
-              TextField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  onChanged: (String value) {
-                    setState(() {
-                      _passwordValue = value;
-                    });
-                  }),
-              SwitchListTile(
-                  value: _acceptTerms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _acceptTerms=value;
-                    });
-                  },
-                  title: Text('Accept Terms')),
-              SizedBox(height: 10.0),
-              RaisedButton(
-                color: Theme.of(context).primaryColor,
-                textColor: Colors.white,
-                child: Text('LOGIN'),
-                onPressed: () {
-                  print(_emailValue);
-                  print(_passwordValue);
-                  Navigator.pushReplacementNamed(context, '/products');
-                },
-              )
-            ])));
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                    decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        filled: true,
+                        fillColor: Colors.white),
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (String value) {
+                      setState(() {
+                        _emailValue = value;
+                      });
+                    }),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        filled: true,
+                        fillColor: Colors.white),
+                    obscureText: true,
+                    onChanged: (String value) {
+                      setState(() {
+                        _passwordValue = value;
+                      });
+                    }),
+                SwitchListTile(
+                    value: _acceptTerms,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _acceptTerms = value;
+                      });
+                    },
+                    title: Text('Accept Terms')),
+                SizedBox(height: 10.0),
+                RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    child: Text('LOGIN'),
+                    onPressed: () {
+                      print(_emailValue);
+                      print(_passwordValue);
+                      Navigator.pushReplacementNamed(context, '/products');
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
