4dc8bc48c2b0a14b21d1a7cc80495a93a8c7729d
tbrams <torben@brams.dk>
2019-01-04 17:04:05 +0100

    Added auth page and cleaned up a lot

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,8 +1,66 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  final Function addProduct;
+
+  ProductCreatePage(this.addProduct);
+
   @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  @override
+  String _titleValue = '';
+  String _descriptionValue = '';
+  double _priceValue;
+
   Widget build(BuildContext context) {
-    return Center(child: Text('Create a Product'),);
+    return Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Title'),
+            onChanged: (String value) {
+              setState(() {
+                _titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(labelText: 'Product Description'),
+            onChanged: (String value) {
+              setState(() {
+                _descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Product Price'),
+            onChanged: (String value) {
+              setState(() {
+                _priceValue = double.parse(value);
+              });
+            },
+          ),
+          SizedBox(height: 10.0),
+          RaisedButton(
+            child: Text('Save'),
+            onPressed: () {
+              final Map<String, dynamic> product = {
+                'title': _titleValue,
+                'description': _descriptionValue,
+                'price': _priceValue,
+                'image':'assets/food.jpg'
+              };
+              widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/products');
+            },
+          )
+        ]));
   }
 }
